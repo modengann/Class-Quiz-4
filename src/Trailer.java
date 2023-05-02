ac7d86edc0137b497123f8bbffb4c174e6ad7a39
casey <cpmoden@gmail.com>
2023-05-02 18:49:02 -0400

    Update trailer class

--- src/Trailer.java
+++ src/Trailer.java
@@ -1,30 +1,38 @@
+//Trailer Class, DO NOT CHANGE!
+
 public class Trailer {
     private double lightQuality;
     private double roomQuality;
     private int capacity;
 
+    //Trailer constructor, receives information about capacity, light quality, and room quality
     public Trailer(int c, double l, double r){
         capacity = c;
         lightQuality = l;
         roomQuality = r;
     }
 
+    //Set light quality
     public void setLightQuality(double l){
         lightQuality = l;
     }
 
-    public int getCapacity(){
-        return capacity;
-    }
-
+    //Get light quality
     public double getLightQuality(){
         return lightQuality;
     }
 
+    //Get capacity
+    public int getCapacity(){
+        return capacity;
+    }
+
+    //Get room quality
     public double getRoomQuality(){
         return roomQuality;
     }
 
+    //Set room quality
     public void setRoomQuality(double r){
         roomQuality = r;
     }
